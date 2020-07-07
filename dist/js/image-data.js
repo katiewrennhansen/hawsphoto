611a979dcb96c4acde2e4aa33bf8dd7095128ebf
Katie Hansen <khansen0415@gmail.com>
2020-07-07 07:08:32 -0600

    add new photos and photo names

--- dist/js/image-data.js
+++ dist/js/image-data.js
@@ -3,135 +3,145 @@ const images = [
         id: 1,
         name: 'Illusion',
         imagePath: 'images/illusion.jpg',
-        description: 'testing the json-like data structure',
-        price: ['101', '200'],
-        sizes: ['13x15', '40x80'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 2,
         name: 'Milky Way',
         imagePath: 'images/milky_way.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16', '16x32'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 3,
         name: 'Painted Walk',
         imagePath: 'images/painted_walk.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 4,
         name: 'Rocky Falls',
         imagePath: 'images/rocky_falls.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 5,
         name: 'Shape Shifter',
         imagePath: 'images/shape_shifter.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 6,
         name: 'Shore Side',
         imagePath: 'images/shore_side.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 7,
         name: 'The Awakening',
         imagePath: 'images/the_awakening.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 8,
         name: 'The Glow',
         imagePath: 'images/the_glow.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 9,
         name: 'The Shallows',
         imagePath: 'images/the_shallows.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 10,
-        name: 'Untitled',
-        imagePath: 'images/unititled.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        name: 'Rise',
+        imagePath: 'images/rise.jpg',
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 11,
         name: 'Waves',
         imagePath: 'images/waves.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 12,
         name: 'Be Bold-er',
         imagePath: 'images/be_bolder.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 13,
         name: 'Blue Hour',
         imagePath: 'images/blue_hour.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 14,
         name: 'Burn Bright',
         imagePath: 'images/burn_bright.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     },
     {
         id: 15,
-        name: 'Untitled 2',
-        imagePath: 'images/untitled_2.jpg',
-        description: 'testing the json-like data structure again',
-        price: ['101', '200'],
-        sizes: ['13x16'],
-        layout: ['Portrait', 'Landscape']
+        name: 'Just Flow With It',
+        imagePath: 'images/just_flow_with_it.jpg',
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
+    },
+    ,
+    {
+        id: 16,
+        name: 'Space Out',
+        imagePath: 'images/space_out.jpg',
+        description: '',
+        price: [],
+        sizes: [],
+        layout: []
     }
 ]
